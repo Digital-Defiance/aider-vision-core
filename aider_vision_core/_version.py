dc0679cbd1506e7d7a84c000ae1553263d821e63
Jessica Mulein <jessica@digitaldefiance.org>
2026-05-24 10:18:05 -0700

    v0.90.8.dev0

--- aider_vision_core/_version.py
+++ aider_vision_core/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.90.7.dev0'
-__version_tuple__ = version_tuple = (0, 90, 7, 'dev0')
+__version__ = version = '0.90.8.dev0'
+__version_tuple__ = version_tuple = (0, 90, 8, 'dev0')
 
-__commit_id__ = commit_id = 'gd0a7f6a71'
+__commit_id__ = commit_id = 'g3b5ad9f66'
