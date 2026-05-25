789ead8aa2d856f0517aa2faa28d66387bfcd955
Jessica Mulein <jessica@digitaldefiance.org>
2026-05-24 21:30:59 -0700

    chore: release v0.90.13.dev0

--- aider_vision_core/_version.py
+++ aider_vision_core/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.90.12.dev0'
-__version_tuple__ = version_tuple = (0, 90, 12, 'dev0')
+__version__ = version = '0.90.13.dev0'
+__version_tuple__ = version_tuple = (0, 90, 13, 'dev0')
 
-__commit_id__ = commit_id = 'gc4589aeaa'
+__commit_id__ = commit_id = 'gf76b6c340'
